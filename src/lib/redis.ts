0cd64a23e75109053ea25d28c12f6923fe74be9b
Raxuis <raclot.raphael@gmail.com>
2024-02-11 22:48:30 +0100

    Feat(back) : Redis use

---
+++ src/lib/redis.ts
@@ -0,0 +1,8 @@
+import { Redis } from "@upstash/redis";
+
+const redis = new Redis({
+  url: "https://eu2-handy-ghoul-31376.upstash.io",
+  token: process.env.REDIS_KEY!,
+});
+
+const data = await redis.set("foo", "bar");
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,4 @@
 import { NextRequest, NextResponse } from "next/server";
-
 export default async function middleware(req: NextRequest) {
   if (req.nextUrl.pathname === "/") {
     // track analytics event
