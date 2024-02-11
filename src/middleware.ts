75012dacf77bcc1d939a484189f03f6e97e3aa39
Raxuis <raclot.raphael@gmail.com>
2024-02-11 22:26:54 +0100

    Feat(back) : track of the user infos

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,8 +4,13 @@ export default async function middleware(req: NextRequest) {
   if (req.nextUrl.pathname === "/") {
     // track analytics event
     try {
+      analytics.track("pageview", {
+        page: "/",
+        country: req.geo?.country,
+      });
     } catch (err) {
       // fail silently
+      console.log(err);
     }
   }
   return NextResponse.next();
