457cb7fc5395fb0ed03a4153e6f4f5736b7d000b
Raxuis <raclot.raphael@gmail.com>
2024-02-11 23:12:50 +0100

    Bug : Await

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -1,6 +1,6 @@
 import { Redis } from "@upstash/redis";
 
-export const redis = new Redis({
+const redis = new Redis({
   url: "https://eu2-handy-ghoul-31376.upstash.io",
   token: process.env.REDIS_KEY!,
 });
