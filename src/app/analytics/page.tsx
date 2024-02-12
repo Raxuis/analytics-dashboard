a2e7a62842b86c84e888016e7184177e9a64b98f
Raxuis <raclot.raphael@gmail.com>
2024-02-12 21:34:46 +0100

    Feat(front) : Amount of visitors today implementation

--- src/app/analytics/page.tsx
+++ src/app/analytics/page.tsx
@@ -1,12 +1,13 @@
 import AnalyticsDashboard from "@/components/AnalyticsDashboard";
+import { getDate } from "@/utils";
 import { analytics } from "@/utils/analytics";
 
 const Page = async () => {
   const TRACKING_DAYS = 7;
 
-  const pageview = await analytics.retrieveDays("pageview", TRACKING_DAYS);
+  const pageviews = await analytics.retrieveDays("pageview", TRACKING_DAYS);
 
-  const totalPageViews = pageview.reduce((acc, curr) => {
+  const totalPageViews = pageviews.reduce((acc, curr) => {
     return (acc + curr.events.reduce((acc, curr) => {
       return acc + Object.values(curr)[0]!
     }, 0)
@@ -15,9 +16,15 @@ const Page = async () => {
 
   const avgVisitorsPerDay = (totalPageViews / TRACKING_DAYS).toFixed(1);
 
+  const amtVisitorsToday = pageviews.filter((ev) => ev.date === getDate()).reduce((acc, curr) => {
+    return (
+      acc + curr.events.reduce((acc, curr) => acc + Object.values(curr)[0]!, 0)
+    )
+  }, 0);
+
   return <div className="min-h-screen w-full py-12 flex justify-center items-center">
     <div className="relative w-full max-w-6xl mx-auto text-white">
-      <AnalyticsDashboard avgVisitorsPerDay={avgVisitorsPerDay} />
+      <AnalyticsDashboard avgVisitorsPerDay={avgVisitorsPerDay} amtVisitorsToday={amtVisitorsToday} />
     </div>
   </div>;
 };
