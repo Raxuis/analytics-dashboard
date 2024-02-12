0b24e95c895363f5e6e55f4d78d8d19e79821253
Raxuis <raclot.raphael@gmail.com>
2024-02-12 21:01:01 +0100

    Feat(front) : Analytics Dashboard component in /analytics

--- src/app/analytics/page.tsx
+++ src/app/analytics/page.tsx
@@ -3,7 +3,11 @@ import { analytics } from "@/utils/analytics";
 const Page = async () => {
 
   const pageview = await analytics.retrieveDays("pageview", 2);
-  return <pre className="text-white">{JSON.stringify(pageview)}</pre>;
+  return <div className="min-h-screen w-full py-12 flex justify-center items-center">
+    <div className="relative w-full max-w-6xl mx-auto text-white">
+      <AnalyticsDashboard />
+    </div>
+  </div>;
 };
 
 export default Page;
