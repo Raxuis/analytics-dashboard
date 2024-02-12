d1ff0f05becb1e42b6509e0b6ff8d0f4ae586ffc
Raxuis <raclot.raphael@gmail.com>
2024-02-12 21:23:28 +0100

    Feat(front) : Average visitors per day implementation

--- src/app/analytics/page.tsx
+++ src/app/analytics/page.tsx
@@ -1,11 +1,23 @@
+import AnalyticsDashboard from "@/components/AnalyticsDashboard";
 import { analytics } from "@/utils/analytics";
 
 const Page = async () => {
+  const TRACKING_DAYS = 7;
 
-  const pageview = await analytics.retrieveDays("pageview", 2);
+  const pageview = await analytics.retrieveDays("pageview", 7);
+
+  const totalPageViews = pageview.reduce((acc, curr) => {
+    return (acc + curr.events.reduce((acc, curr) => {
+      return acc + Object.values(curr)[0]!
+    }, 0)
+    );
+  }, 0);
+
+  const avgVisitorsPerDay = (totalPageViews / TRACKING_DAYS).toFixed(1);
+
   return <div className="min-h-screen w-full py-12 flex justify-center items-center">
     <div className="relative w-full max-w-6xl mx-auto text-white">
-      <AnalyticsDashboard />
+      <AnalyticsDashboard avgVisitorsPerDay={avgVisitorsPerDay} />
     </div>
   </div>;
 };
