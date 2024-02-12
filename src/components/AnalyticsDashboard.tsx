5232ffbc6a3478aa9e84ab8c070cf718c1c316d3
Raxuis <raclot.raphael@gmail.com>
2024-02-12 22:12:09 +0100

    Feat(front) : Section for country with flags of visitors

--- src/components/AnalyticsDashboard.tsx
+++ src/components/AnalyticsDashboard.tsx
@@ -1,13 +1,15 @@
 import { analytics } from "@/utils/analytics";
 import { Card, BarChart } from "@tremor/react";
+import ReactCountryFlag from "react-country-flag";
 
 interface AnalyticsDashboardProps {
   avgVisitorsPerDay: string
   amtVisitorsToday: number
   timeseriesPageviews: Awaited<ReturnType<typeof analytics.retrieveDays>>
+  topCountries: [string, number][]
 }
 
-const AnalyticsDashboard = ({ avgVisitorsPerDay, amtVisitorsToday, timeseriesPageviews }: AnalyticsDashboardProps) => {
+const AnalyticsDashboard = ({ avgVisitorsPerDay, amtVisitorsToday, timeseriesPageviews, topCountries }: AnalyticsDashboardProps) => {
   return <div className="flex flex-col gap-6">
     <div className="grid w-full mx-auto grid-cols-1 sm:grid-cols-2 gap-6">
       <Card className="w-full mx-auto max-w-xs">
@@ -19,6 +21,25 @@ const AnalyticsDashboard = ({ avgVisitorsPerDay, amtVisitorsToday, timeseriesPag
         <p className="text-3xl text-dark-tremor-content-strong font-semibold">{amtVisitorsToday}</p>
       </Card>
     </div>
+
+    <Card className="flex flex-col sm:grid grid-cols-4 gap-6">
+      <h2 className="w-full text-dark-tremor-content-strong text-center sm:left-left font-semibold text-xl">
+        This weeks top visitors:
+      </h2>
+      <div className="col-span-3 flex items-center justify-between flex-wrap gap-8">
+        {topCountries?.map(([countryCode, number]) => {
+          return (
+            <div key={countryCode} className="flex items-center gap-3 text-dark-tremor-content-strong"><p className="hidden sm:block text-tremor-content">
+              {countryCode}
+            </p>
+              <ReactCountryFlag className="text-5xl sm:text-3xl" countryCode={countryCode} svg />
+              <p className="text-tremor-content sm:text-dark-tremor-content-strong">{number}</p>
+            </div>
+          )
+        })}
+      </div>
+    </Card>
+
     <Card>
       {timeseriesPageviews ? (
         <BarChart allowDecimals={false} showAnimation data={
