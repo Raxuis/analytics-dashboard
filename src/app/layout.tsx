50141b6a69536fba404d61a37c9becf2dd337008
Raxuis <raclot.raphael@gmail.com>
2024-02-12 23:23:11 +0100

    Fix(front) : edited metadat favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,10 @@ export const metadata: Metadata = {
   title: "Raphaël | Analytics",
   description:
     "An Analytics Dashboard Website made by Raphaël | Raxuis to check his stats!",
+  keywords: "Raphaël, Raxuis, Analytics, Next.js, React.js",
+  icons: {
+    icon: '/favicon.ico',
+  },
 };
 
 export default function RootLayout({
