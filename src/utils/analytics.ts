3d44d57319a5b70c70aea89dc44a7e843a3c4e00
Raxuis <raclot.raphael@gmail.com>
2024-02-12 20:06:37 +0100

    Feat(full) : Visit on the website sent to the DB

--- src/utils/analytics.ts
+++ src/utils/analytics.ts
@@ -1,9 +1,13 @@
 import { redis } from "@/lib/redis";
+import { getDate } from "@/utils";
 
 type AnalyticsArgs = {
   retention?: number;
 };
 
+type TrackOptions = {
+  persist?: boolean;
+}
 export class Analytics {
   private retention: number = 60 * 60 * 24 * 7;
 
@@ -11,11 +15,16 @@ export class Analytics {
     if (opts?.retention) this.retention = opts.retention;
   }
 
-  async track(namespace: string, event: object = {}) {
-    const key = `analytics::${namespace}`;
+  async track(namespace: string, event: object = {}, opts?: TrackOptions) {
+    let key = `analytics::${namespace}`;
+
+    if (!opts?.persist) {
+      key += `::${getDate()}`
+    }
 
     // db call to persist this event
     await redis.hincrby(key, JSON.stringify(event), 1)
+    if (!opts?.persist) await redis.expire(key, this.retention);
   }
 }
-export const analytics = new Analytics({ retention: 3600 });
+export const analytics = new Analytics();
